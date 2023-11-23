7b8143660d1c0e9f2ac1b547c131f624e5297a8a
Gemego <ziangzhang@qq.com>
2023-11-23 16:22:46 +0800

    lab1 sleep done

--- user/sleep.c
+++ user/sleep.c
@@ -4,5 +4,16 @@
 
 int main(int argc, char *argv[])
 {
-    
+    if (argc == 2)
+    {
+        int ticks = atoi(argv[1]);
+        sleep(ticks);
+    }
+    else
+    {
+        fprintf(2, "usage: sleep [num of ticks]\n");
+        fprintf(2, "argc = %d\n", argc);
+        exit(1);
+    }
+    exit(0);
 }

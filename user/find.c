ebf76a6924c783d466114be47d35994578ae2dcb
Gemego <ziangzhang@qq.com>
2023-11-27 00:33:30 +0800

    need to be recursionable

--- user/find.c
+++ user/find.c
@@ -4,17 +4,62 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+char* fmtname(char *);
+char* get_filename(char *);
+void recur_find(char *, char *);
+
 int main(int argc, char *argv[])
 {
     if(argc < 3)
     {
-        fprintf(2, "usage: find [name of files]\n");
+        fprintf(2, "usage: find [path] [name of files]\n");
         exit(1);
     }
 
-    char *path = argv[1];
-    char *name = argv[2];
+    recur_find(argv[1], argv[2]);
+    exit(0);
+}
 
+char* fmtname(char *path)
+{
+    static char buf[DIRSIZ + 1];
+    char *p;
+
+    // Find first character after last slash.
+    for(p = path + strlen(path); p >= path && *p != '/'; p--)
+    ;
+    p++;
+
+    // Return blank-padded name.
+    if(strlen(p) >= DIRSIZ)
+        return p;
+
+    memmove(buf, p, strlen(p));
+    memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
+    return buf;
+}
+
+char* get_filename(char *path)
+{
+    static char buf[DIRSIZ + 1];
+    char *p;
+
+    // Find first character after last slash.
+    for(p = path + strlen(path); p >= path && *p != '/'; p--)
+    ;
+    p++;
+
+    // Return blank-padded name.
+    if(strlen(p) >= DIRSIZ)
+        return p;
+
+    memmove(buf, p, strlen(p));
+
+    return buf;
+}
+
+void recur_find(char *path, char *name)
+{
     char buf[512], *p;
     int fd;
     struct dirent de;
@@ -33,5 +78,36 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    
+    switch(st.type)
+    {
+    case T_DEVICE:
+    case T_FILE:
+        if (strcmp(get_filename(path), name) == 0)
+        {
+            printf("%s\n", path);
+        }
+        break;
+
+    case T_DIR:
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+        {
+            printf("ls: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+    }
+    break;
+  }
 }

94ef6ce04c0cabf811f33cf2e03fde74de5019a2
Gemego <ziangzhang@qq.com>
2023-11-28 12:16:36 +0800

    xargs.c done, wait to be tested

--- user/xargs.c
+++ user/xargs.c
@@ -5,10 +5,29 @@
 
 int main(int argc, char *argv[])
 {
-    char buf[512];
+    char output_buf[512];
+    // char *loc_argv[MAXARG];
+
     if (fork() == 0)
     {
-        // read()
+        char *p, *t;
+        p = t = output_buf;
+
+        while (read(0, p++, 1) == 1)
+        {
+            if (*(p - 1) == '\n')
+            {
+                *(p - 1) == '\0';
+                int len = strlen(t);
+                char *tem_argv = (char *)malloc(len);
+                memcpy(tem_argv, t, len);
+
+                exec(argv[1], &tem_argv);
+
+                t = t + len + 1;
+            }
+        }
+        exit(0);
     }
     
     wait((int *)0);

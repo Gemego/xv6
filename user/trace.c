f8d9cf65bc0b237ffb3493484726403a55f089d3
Gemego <ziangzhang@qq.com>
2023-12-04 20:21:25 +0800

    start writing user/trace.c

--- user/trace.c
+++ user/trace.c
@@ -4,5 +4,14 @@
 
 int main(int argc, char *argv[])
 {
-    exit(0);
+    if (argc > 2)
+    {
+        uint32 mask = (uint32)atoi(argv[1]);
+        
+    }
+    else
+    {
+        fprintf(2, "usage: trace [system calls mask] [cmd]\n");
+        exit(1);
+    }
 }

86ce263ae24c420d569591372832e0dc16f26578
Gemego <ziangzhang@qq.com>
2023-12-16 19:16:06 +0800

    print arg in vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -452,6 +452,6 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 
 int vmprint(pagetable_t pagetable)
 {
-  printf("page table %x\n", (uint64) pagetable);
+  printf("page table %p\n", pagetable);
   return 0;
 }
